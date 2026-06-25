9a66eba2576348e2ea09b799bba6d72e357d40c7
tbdavid2019 <tbdavid2019@gmail.com>
2026-06-25 11:06:19 +0800

    更改 聯繫人

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,17 +1,17 @@
 export default defineAppConfig({
   title: 'glsoft.ai',
-  email: 'ok@vip.david888.com',
+  email: '104@david888.com',
   github: 'https://github.com/tbdavid2019/sink',
   twitter: 'https://x.com/oobwei',
   telegram: 'https://t.me/a6a7a8a9abc',
-  discord: 'https://discord.gg/sink',
+  discord: 'https://discord.gg/9BCGcgCWpj',
   blog: 'https://blog.david888.com',
-  description: '短網址e.',
+  description: '短網址',
   image: 'https://blog.david888.com/banner.png',
   company: {
     name: '資旅軟體開發有限公司',
-    nameEnglish: 'glsoft.ai',
-    taxId: '00000000',
+    nameEnglish: 'glsoft.ai / 維護者信箱 104@david888.com',
+    taxId: '90867427',
     representative: 'HUNG FUNG CHAK',
     address: '臺北市信義區松德路171號9樓之2',
     addressEnglish: '9 F.-2, No. 171, Songde Rd., Xinyi Dist., Taipei City 110030, Taiwan (R.O.C.)'
